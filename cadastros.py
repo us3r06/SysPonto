bc70ecb5075fbdc55db6ee09d10479a7134f2ad2
us3r06 <hdanyllo@ufrn.edu.br>
2018-11-13 22:12:23 -0300

    menu cadastros

--- cadastros.py
+++ cadastros.py
@@ -1,8 +1,38 @@
 import validacoes
 import SysPonto
 
+def centralizar(a):
+	print(" "+a.center(70)+" ")
+
 def menuCadastros():
-	
+	menu = "opções de Cadastros"
+	escolha = "Escolha a opção desejada"
+	a = "1-Cadastrar Professor"
+	b = "2-Cadastrar Diretor"
+	c = "3-Cadastrar DIRED"
+	e = "0-Sair"
+	centralizar(menu)
+	centralizar(escolha)
+	centralizar(a)
+	centralizar(b)
+	centralizar(c)
+	centralizar(e)
+def executeCadastros():
+	while True:
+		menuCadastros()
+		opcaoLogin = input("Digite a opção desejada: ")
+		if opcaoLogin.strip() == '0':
+			print("Obrigado por usar o SysPonto")
+			sys.exit()
+		elif opcaoLogin.strip() == '1':
+			cadastraProfessor()
+		elif opcaoLogin.strip() == '2':
+			cadastraDiretor
+		elif opcaoLogin.strip() == '3':
+			cadastraDired()
+		else:
+			print(opcaoLogin, "não é uma opção válida!\n")
+
 
 def cadastraProfessor():
 	nome =input("Digite o seu nome: ")
@@ -27,7 +57,7 @@ def cadastraProfessor():
 		estado = input("Estado Inválido.\nDigite o seu estado: ")
 	nome= nome
 	SysPonto.professores[cpf] = [nome,fone,cpf,email,nasc,cidade,estado]
-	grava_clientes()
+	##grava_clientes()
 	print(clientes)
 	print("Professor cadastrado com sucesso")
 	os.system ("cls")
@@ -53,8 +83,25 @@ def cadastraDiretor():
 	nmrDired = input("Digite o número da DIRED a qual sua escola pertence: ")
 	nome= nome
 	SysPonto.Diretores[cpf] = [nome,fone,cpf,email,nasc,cidade,nmrDired]
-	grava_diretor()
+	##grava_diretor()
 	print(diretores)
 	print("Diretor cadastrado com sucesso")
 	os.system ("cls")
 
+
+def cadastraDired():
+	nome =input("Digite o Código da DIRED: ")
+	while validacoes.validafone(fone) == False:
+		fone =input("Digite o seu celular = xxxxxxxxxxx ")
+		fone.split(' ')
+	validacoes.validafone(fone)
+	
+	email =input("Digite o seu email: ")
+	while validacoes.validaemail(email) == False:
+		email =input("Email inexistente \nDigite o seu email: ")
+	cidade =input("Digite a sua cidade: ")
+	SysPonto.Diretores[cod] = [fone,email,cidade]
+	##grava_dired()
+	print(diretores)
+	print("DIRED cadastrada com sucesso")
+	os.system ("cls")
